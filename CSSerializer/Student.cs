45b21a61128196d27cb6fbf97871d234b9ba0a92
d7p4n4 <bercikezs199999999@gmail.com>
2019-10-23 10:34:01 +0100

    1.20191023.1

--- CSSerializer/Student.cs
+++ CSSerializer/Student.cs
@@ -11,6 +11,8 @@ namespace CSSerializer
     //[GUID()]
     class Student
     {
+        public int id { get; set; }
+        public string GUID { get; set; }
         public int age { get; set; }
         public string name { get; set; }
         public string address { get; set; }
